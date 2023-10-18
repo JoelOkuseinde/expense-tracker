9b1d29af3819d1980e10cd2f5a0fe505ab1d97a2
JoelOkuseinde <joelokuseinde456@gmail.com>
2023-10-18 18:46:56 +0100

    2nd commit, completed half of the backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+
+// Load environment variables from config.env file
+dotenv.config({ path: './config/config.env' });
+
+const transactions = require('./routes/transactions');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/transactions', transactions);
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
