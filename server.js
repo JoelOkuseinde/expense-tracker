39c89eee787c94b9d1a6a9f0cb517c44dbc31b5d
JoelOkuseinde <joelokuseinde456@gmail.com>
2023-10-18 19:01:25 +0100

    2nd commit, completed half of the backend.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db');
 // Load environment variables from config.env file
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
